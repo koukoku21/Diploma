0424d2704e09f40c04b440f244edb17f955c085d
unknown <230198@astanait.edu.kz>
2026-03-31 23:40:06 +0500

    update NDK version to match plugin requirements

--- apps/mobile/android/app/build.gradle.kts
+++ apps/mobile/android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "kz.miraku.miraku"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
